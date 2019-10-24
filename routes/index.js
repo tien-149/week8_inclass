bb9e85a62a7939fb28625a9e53edb46813451d2f
Le Thi Cam Tien <thicamtien.le@users-iMac.fc.ca>
2019-10-24 13:14:32 -0400

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,24 @@ router.get('/', (req, res) => {
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
+        // console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        res.render('home', { people: result }); //we can change the key but we had to keep the result
+    })
+})
+
+router.get('/:id', (req, res) => {
+    // should really get the user data here and then fetch it thru, but let's try this asynchronously
+    console.log('at the main route');
+    console.log(req.params.id); //1 2 3 or whatever comes after the slach
+
+    let query = `select * from tbl_bio where profID="${req.params.id}"`;
+
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
         console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
@@ -21,3 +39,9 @@ router.get('/', (req, res) => {
 })
 
 module.exports = router;
+
+
+//type show table => show 2 table;
+// describe tablename
+// Change the data for that table
+//updata table_myadmin
