88e9ce0232e43f3b7ff8ff53b7e41403e6b65ee5
Le Thi Cam Tien <thicamtien.le@users-iMac.fc.ca>
2019-11-07 12:04:15 -0500

    add error page

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const sql = require('../utils/sql');
+const sql = require("../utils/sql");
 
-router.get('/', (req, res) => {
-    // should really get the user data here and then fetch it thru, but let's try this asynchronously
-    console.log('at the main route');
+router.get("/", (req, res) => {
+  // should really get the user data here and then fetch it thru, but let's try this asynchronously
+  console.log("at the main route");
 
-    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
+  let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
 
+  sql.query(query, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    }
 
-    sql.query(query, (err, result) => {
-        if (err) { throw err; console.log(err); }
+    console.log(result); // should see objects wrapped in an array
 
-        // console.log(result); // should see objects wrapped in an array
+    // render the home view with dynamic data
+    res.render("home", { people: result });
+  });
+});
 
-        // render the home view with dynamic data
-        res.render('home', { people: result }); //we can change the key but we had to keep the result
-    })
-})
+router.get("/users/:id", (req, res) => {
+  // should really get the user data here and then fetch it thru, but let's try this asynchronously
+  console.log("at the user route");
+  console.log(req.params.id); // 1 2 3 or whatever comes after the slash
 
-router.get('/:id', (req, res) => {
-    // should really get the user data here and then fetch it thru, but let's try this asynchronously
-    console.log('at the main route');
-    console.log(req.params.id); //1 2 3 or whatever comes after the slach
+  let query = `select * from tbl_bio where profID="${req.params.id}"`;
 
-    let query = `select * from tbl_bio where profID="${req.params.id}"`;
+  sql.query(query, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    }
 
+    console.log(result); // should see objects wrapped in an array
 
-    sql.query(query, (err, result) => {
-        if (err) { throw err; console.log(err); }
+    //convert the social property into an array
+    //before we send it thru
+    //map is an array method that let you map one value to another ( convert it )
+    result[0].social = result[0].social.split(",").map(function(item) {
+      item = item.trim();
+      //item.trim() removes any empty white space from text
+      return item;
+    });
 
-        console.log(result); // should see objects wrapped in an array
-            // convert the social property into an array before you send it thru
-        // render the home view with dynamic data
-        result[0].social=result[0].social.split(",").map(function(item){
-            item = item.trim();
-            //item.trim() remove any empty white space from the text
+    console.log("after trim/ conversion:", result[0]);
 
-            return item;
-        })
-
-        console.log("after trim/conversion:", result[0]);
-        //remder the home view with dynamic data
-        res.json(result[0]);
-
-        // res.render('home', { people: result });
-         //we can change the key but we had to keep the result
-    })
-})
+    // render the home view with dynamic data
+    res.json(result[0]);
+  });
+});
 
 module.exports = router;
-
-
-//type show table => show 2 table;
-// describe tablename
-// Change the data for that table
-//updata table_myadmin
